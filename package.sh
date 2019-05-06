fbd0b3cde3cf8863cd6f777c0d0c55f3831bc132
Adam Wiethuechter <adam.wiethuechter@critical.com>
2019-05-05 22:52:41 -0400

    Packager updated
    Reworked the packager script to be cleaner, then zipped up
    and placed new version into base-build for CTI.

--- package.sh
+++ package.sh
@@ -11,15 +11,16 @@ fi
 
 # grab the commit id
 commit=$(git log --oneline -1)
+package_name=cti-openhip_$(date +%Y%m%d)_${commit:0:7}
 
 # create the package manually
-mkdir cti-openhip_$(date +%Y%m%d)_${commit:0:7}
-echo $(date +%Y%m%d)_${commit:0:7} > cti-openhip_$(date +%Y%m%d)_${commit:0:7}/VERSION
-cp -r usr-local-sbin/ docs/ install.sh INSTALL cti-openhip_$(date +%Y%m%d)_${commit:0:7}
+mkdir $package_name
+echo $(date +%Y%m%d)_${commit:0:7} > $package_name/VERSION
+cp -r usr-local-sbin/ docs/ install.sh INSTALL $package_name/
 
 # run dos2unix to clean up anything nasty
-find cti-openhip_$(date +%Y%m%d)_${commit:0:7} -type f -print0 | xargs -0 dos2unix
+find $package_name/ -type f -print0 | xargs -0 dos2unix
 
 # zip up the package with name and date and remove the folder to leave zip
-zip -r cti-openhip_$(date +%Y%m%d)_${commit:0:7}.zip cti-openhip_$(date +%Y%m%d)_${commit:0:7}
-rm -rf cti-openhip_$(date +%Y%m%d)_${commit:0:7}/
+zip -r $package_name.zip $package_name/
+rm -rf $package_name/
