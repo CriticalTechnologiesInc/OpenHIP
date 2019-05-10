ab11a1622654a1da8f0462039b5962b3fefac6cb
Adam Wiethuechter <adam.wiethuechter@critical.com>
2019-05-10 14:18:31 -0400

    Add DNS creation to install
    Script when using the "-s" parameter will also expect the FQDN of
    the server (hostname.domain.com) to create a DNS entry for the
    system.
    
    ./install -s host.domain.com
    
    This will create a file called "my_hi2dns" in /usr/local/sbin.
    Its contents can be added to a DNS servers entries for the Type 55
    lookup on that DNS.

--- install.sh
+++ install.sh
@@ -9,9 +9,10 @@ cd /usr/local/sbin/
 
 if [[ $1 == "-s" ]]
 then
-	sudo ./hitgen
+	sudo ./hitgen -name $2 -noinput
 	sudo ./hitgen -conf
 	sudo ./hitgen -publish
 	sudo chmod 755 /usr/local/etc/hip
 	sudo mv hip.conf *.pub.xml /usr/local/etc/hip/
+	sudo hipdns -d ../etc/hip/my_host_identites.xml
 fi
